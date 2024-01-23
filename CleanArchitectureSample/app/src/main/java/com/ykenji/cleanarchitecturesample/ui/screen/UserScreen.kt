82e17a34a824de4667375945fc764ad50288cea6
Kenji Yokoyama <Kenji.Yokoyama@sony.com>
2024-01-23 15:55:03 +0900

    Show user role in UI

--- CleanArchitectureSample/app/src/main/java/com/ykenji/cleanarchitecturesample/ui/screen/UserScreen.kt
+++ CleanArchitectureSample/app/src/main/java/com/ykenji/cleanarchitecturesample/ui/screen/UserScreen.kt
@@ -3,6 +3,8 @@ package com.ykenji.cleanarchitecturesample.ui.screen
 import android.widget.Toast
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -20,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.ykenji.cleanarchitecturesample.R
@@ -150,6 +153,8 @@ fun UserList(
                     }
                 )
                 Text(userData.name)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(userData.role.name)
             }
         }
     }
