3b70620cd582517ed600243aedfd4223d07114f5
Kenji Yokoyama <Kenji.Yokoyama@sony.com>
2024-02-01 23:38:15 +0900

    Change class name of in-memory user repository

--- CleanArchitectureSample/app/src/androidTest/java/com/ykenji/cleanarchitecturesample/repository/user/InMemoryUserRepositoryTest.kt
+++ CleanArchitectureSample/app/src/androidTest/java/com/ykenji/cleanarchitecturesample/repository/user/InMemoryUserRepositoryTest.kt
@@ -3,7 +3,7 @@ package com.ykenji.cleanarchitecturesample.repository.user
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.ykenji.cleanarchitecturesample.config.inject.RepositoryModule
 import com.ykenji.cleanarchitecturesample.domain.adapter.repository.user.UserRepository
-import com.ykenji.cleanarchitecturesample.infrastructure.datasource.arraylist.repository.user.ArrayListUserRepository
+import com.ykenji.cleanarchitecturesample.infrastructure.datasource.inmemory.repository.user.InMemoryUserRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -21,7 +21,7 @@ import javax.inject.Singleton
 @RunWith(AndroidJUnit4::class)
 @UninstallModules(RepositoryModule::class)
 @HiltAndroidTest
-class ArrayListUserRepositoryTest {
+class InMemoryUserRepositoryTest {
 
     @get:Rule
     var hiltRule = HiltAndroidRule(this)
@@ -32,11 +32,11 @@ class ArrayListUserRepositoryTest {
 
         @Singleton
         @Binds
-        abstract fun bindUserRepository(impl: ArrayListUserRepository): UserRepository
+        abstract fun bindUserRepository(impl: InMemoryUserRepository): UserRepository
     }
 
     @Inject
-    lateinit var userRepository: ArrayListUserRepository
+    lateinit var userRepository: InMemoryUserRepository
 
     @Before
     fun init() {
